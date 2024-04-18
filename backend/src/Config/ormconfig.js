2c450ecde0a6d938ba5e74f46c28c8044aeca347
DekuFoo <ggmaster039@gmail.com>
2024-04-18 12:40:43 +0300

    added .env ormpath

--- backend/src/Config/ormconfig.js
+++ backend/src/Config/ormconfig.js
@@ -1,9 +1,10 @@
 const { Pool } = require("pg");
+require('dotenv').config({path: '../src/Config/ormpath.env'});
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "TechArtisan",
-  password: "1234",
-  port: 5432
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
 });
 module.exports = pool;
